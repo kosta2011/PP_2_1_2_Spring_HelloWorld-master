4adbf72ef30d2ebb72e1517d76d8bce35c1f2bb6
kosta2011 <kosta4job@yandex.ru>
2022-02-19 20:18:31 +0500

    v0.1

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+    //Создайте POJO-класс Cat.
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
